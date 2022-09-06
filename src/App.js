bb0de2ddc51dd9227382978b638ebc20c5ae9e16
Juhwa Hwang <zerothhan@gmail.com>
2022-09-06 15:45:08 +0900

    3. 프로젝트 시작 - 기본 라이브러리 설치

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// 1. 3개 페이지 필요. 홈페이지, movie 페이지, movie detail 페이지
+// 2. 홈페이지에서 배너를 볼 수 있다.
+// 3. 3가지 섹션의 영화를 볼 수 있다. (popular, top rated, upcoming)
+// 4. 각 영화에 마우스를 올리면 제목, 장르, 점수, 인기도, 청불 여부가 노출된다.
+// 5. 영화를 슬라이드로 넘기면서 볼 수 있다.
+// 6. 영화 디테일 페이지에서 영화에 대한 디테일 정보를 볼 수 있다. (포스터, 제목, 줄거리, 점수, 인기도, 청불 여부, 예산, 이익, 러닝 타임 등등)
+// 7. trailer를 누르면 trailer를 볼 수 있다.
+// 8. 영화의 리뷰를 볼 수 있다.
+// 9. 관련된 영화를 볼 수 있다.
+// 10. 영화 검색을 할 수 있다.
+// 11. 영화 정렬을 할 수 있다.
+// 12. 영화를 필터링 할 수 있다.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
