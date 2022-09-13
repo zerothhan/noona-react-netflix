278a162ea9c77411618e32f88746adaca5c859d7
Juhwa Hwang <zerothhan@gmail.com>
2022-09-13 14:28:41 +0900

    9. 환경변수

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -1,9 +1,10 @@
 import api from "../api";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 function getMovies() {
   return async (dispatch) => {
     const popularMovieApi = await api.get(
-      `/movie/popular?api_key=<<api_key>>&language=en-US&page=1`
+      `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
 
     // let response = await fetch(url);
