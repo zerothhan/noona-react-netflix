694cf1aaa39df001a39a2b8378a5e69e0ae5af53
Juhwa Hwang <zerothhan@gmail.com>
2022-09-13 14:57:48 +0900

    10. Promise : API 여러개를 동시에 부르는 법

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -1,18 +1,30 @@
 import api from "../api";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+
 function getMovies() {
   return async (dispatch) => {
-    const popularMovieApi = await api.get(
+    const popularMovieApi = api.get(
       `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
 
-    // let response = await fetch(url);
-    // let data = await response.json();
+    const topRatedApi = api.get(
+      `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    );
 
-    // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
+    const upComingApi = api.get(
+      `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    );
 
-    // let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
+    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+      popularMovieApi,
+      topRatedApi,
+      upComingApi,
+    ]);
+
+    console.log("popularMovieApi", popularMovies);
+    console.log("topRatedApi", topRatedMovies);
+    console.log("upComingApi", upComingMovies);
   };
 }
 
