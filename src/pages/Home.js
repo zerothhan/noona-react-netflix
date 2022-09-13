2ce7e41782f5dd2e087d2bedf199dfc40d80207b
Juhwa Hwang <zerothhan@gmail.com>
2022-09-13 16:10:30 +0900

    14. 슬라이드 만들기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { movieAction } from "../redux/actions/movieAction";
 import { useDispatch, useSelector } from "react-redux";
 import Banner from "../components/Banner";
+import MovieSlide from "../components/MovieSlide";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -12,9 +13,19 @@ const Home = () => {
   useEffect(() => {
     dispatch(movieAction.getMovies());
   }, []);
+
   return (
     <div>
       {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+
+      <h1>Popular Movie</h1>
+      <MovieSlide movies={popularMovies} />
+
+      <h1>Top Rated Movie</h1>
+      <MovieSlide movies={topRatedMovies} />
+
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upComingMovies} />
     </div>
   );
 };
