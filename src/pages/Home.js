f4afd1fd216871268172e9e2861974f864a1e586
Juhwa Hwang <zerothhan@gmail.com>
2022-09-13 14:16:14 +0900

    8. axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
+import { movieAction } from "../redux/actions/movieAction";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
-  useEffect(() => {}, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(movieAction.getMovies());
+  }, []);
   return <div>Home</div>;
 };
 
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -1,11 +1,20 @@
-function getMovies() {
-  return (dispatch) => {
-    let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`;
-    let response = await fetch(url);
-    let data = await response.json();
+import api from "../api";
 
-    let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
-    
-    let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
+function getMovies() {
+  return async (dispatch) => {
+    const popularMovieApi = await api.get(
+      `/movie/popular?api_key=<<api_key>>&language=en-US&page=1`
+    );
+
+    // let response = await fetch(url);
+    // let data = await response.json();
+
+    // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
+
+    // let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
   };
 }
+
+export const movieAction = {
+  getMovies,
+};
