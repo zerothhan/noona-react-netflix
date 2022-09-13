170eadb8782210e4a0b3b05e77046031c4c19fa2
Juhwa Hwang <zerothhan@gmail.com>
2022-09-13 17:16:47 +0900

    17. 카드 디테일

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Badge } from "react-bootstrap";
 
 const MovieCard = ({ item }) => {
   return (
@@ -11,7 +12,18 @@ const MovieCard = ({ item }) => {
           ")",
       }}
     >
-      MovieCard
+      <div className="overlay">
+        <h1>{item.title}</h1>
+        <div>
+          {item.genre_ids.map((id) => (
+            <Badge bg="danger">{id}</Badge>
+          ))}
+        </div>
+        <div>
+          <span>{item.vote_average}</span>
+          <span>{item.adult ? "청불" : "Under 18"}</span>
+        </div>
+      </div>
     </div>
   );
 };
